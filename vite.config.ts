f7d20b281231390fe501480bccb7b2cc1d38e685
Harshtandel95 <harsh@markweb.in>
2025-03-05 18:59:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
+import {BackEndUrl} from './src/config'
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,12 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api/': {  // for content manager
-        target: 'https://article-composer-server.vercel.app/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/content/, '')
-      },
+      '/api': BackEndUrl ,
     }
   }
 })
